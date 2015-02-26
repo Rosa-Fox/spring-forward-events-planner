2c550f244c3885605f99f94ff49999152c04a07d
Rosa-Fox <rosafox89@gmail.com>
2015-02-26 15:27:03 +0000

    Routed the created action

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-     resources :events, only: [:index, :new]
+     resources :events, only: [:index, :new, :create]
 
   # Example resource route with options:
   #   resources :products do
