005b3e8c34a0682172f787ba8612649fe0165dc3
Rosa-Fox <rosafox89@gmail.com>
2015-02-26 17:05:39 +0000

    Routed event#update action

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-     resources :events, only: [:index, :new, :create]
+     resources :events, only: [:index, :new, :create, :update]
 
   # Example resource route with options:
   #   resources :products do
